5f1344696945baf0005c2dde1b95a07cecac783f
krishnacomputercenter24-cell <krishnacomputercenter24@gmail.com>
2026-01-08 01:24:06 +0530

    added origin

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3002",
+  "https://krishnacomputercenterkasna.com",
+  "https://krishnacomputercenterkasna.com"
 ];
 
 app.use(
